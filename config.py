f896a748f384563b2593c456401f9b26a5024651
hvingen <Harold.van.Ingen@isb.nl>
2025-04-17 23:09:17 +0200

    First commit dir

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+SRC_DIR = PROJECT_ROOT / "src"
+SHAPEFILES_DIR = PROJECT_ROOT / "data" / "shapefiles"
+EXPORT_FOLDER = PROJECT_ROOT / "exports"
